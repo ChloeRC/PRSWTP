15a4d86c2f69a4bc9727841cb29975ff4fad5b79
Hazel <confirmationemailconfirmer@gmail.com>
2016-11-21 09:41:09 -0800

    Key mapped A key.

--- PRSWTP/Assets/Scripts/PlayerScript.cs
+++ PRSWTP/Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,8 @@ public class PlayerScript : MonoBehaviour {
 
     private Rigidbody rb;
     private float gameTicks;
-    public float speed;
+    public float horizSpeed;
+    public float jumpSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -20,9 +21,17 @@ public class PlayerScript : MonoBehaviour {
         if (Input.GetButton(KeyCode.D.ToString()) == true)
         {
             Debug.Log("D key pressed.");
-            //Vector2 velocity = new Vector3(speed, 0);
-            transform.Translate(Vector2.right * Time.deltaTime);
-
+            transform.Translate(Vector2.right * horizSpeed * Time.deltaTime);
+        }
+        else if (Input.GetButton(KeyCode.A.ToString()) == true)
+        {
+            Debug.Log("A key pressed.");
+            transform.Translate(Vector2.left * horizSpeed * Time.deltaTime);
+        }
+        else if (Input.GetButton(KeyCode.W.ToString()) == true)
+        {
+            Debug.Log("W key pressed.");
+            transform.Translate(Vector2.up * jumpSpeed * Time.deltaTime);
         }
 	}
 }
