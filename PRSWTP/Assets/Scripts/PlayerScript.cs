633ec3e23c8011a819214a2fa8cf40e74a65ae2c
Hazel <confirmationemailconfirmer@gmail.com>
2016-11-22 09:40:24 -0800

    Added jumping (properly) and fixed keymapping. You can still double-jump, though.

--- PRSWTP/Assets/Scripts/PlayerScript.cs
+++ PRSWTP/Assets/Scripts/PlayerScript.cs
@@ -7,31 +7,54 @@ public class PlayerScript : MonoBehaviour {
     private float gameTicks;
     public float horizSpeed;
     public float jumpSpeed;
+    public float gravity;
 
-	// Use this for initialization
-	void Start () {
+    private static readonly string RIGHT = "right";
+    private static readonly string LEFT = "left";
+    private static readonly string JUMP = "jump";
+
+    private bool isJumping;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
         gameTicks = 0.0F;
+        rb.freezeRotation = true;
+        isJumping = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         gameTicks += Time.deltaTime;
 
-        if (Input.GetButton(KeyCode.D.ToString()) == true)
+        if (Input.GetButton(RIGHT) == true)
         {
-            Debug.Log("D key pressed.");
+            //Debug.Log("D key pressed.");
             transform.Translate(Vector2.right * horizSpeed * Time.deltaTime);
         }
-        else if (Input.GetButton(KeyCode.A.ToString()) == true)
+        if (Input.GetButton(LEFT) == true)
         {
-            Debug.Log("A key pressed.");
+            //Debug.Log("A key pressed.");
             transform.Translate(Vector2.left * horizSpeed * Time.deltaTime);
         }
-        else if (Input.GetButton(KeyCode.W.ToString()) == true)
+
+        if (Input.GetButtonDown(JUMP) == true && !isJumping)
         {
-            Debug.Log("W key pressed.");
-            transform.Translate(Vector2.up * jumpSpeed * Time.deltaTime);
+            //Debug.Log("W key pressed.");
+            isJumping = true;
+            rb.velocity = Vector2.up * jumpSpeed * Time.deltaTime;
         }
+        if (Input.GetButtonUp(JUMP) == true)
+        {
+            isJumping = false;
+        }
+
+        rb.AddForce(Vector2.down * gravity * rb.mass);
+
+        if (isJumping)
+        {
+
+        }
+        
 	}
 }
