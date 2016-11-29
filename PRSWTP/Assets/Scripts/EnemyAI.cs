c9d9a09f362cc32ec2d363b5eb72d0c6359a344b
Cameron <evilbees@comcast.net>
2016-11-29 09:23:27 -0800

    Added AI movement

--- PRSWTP/Assets/Scripts/EnemyAI.cs
+++ PRSWTP/Assets/Scripts/EnemyAI.cs
@@ -6,6 +6,9 @@ public class EnemyAI : MonoBehaviour {
     private Rigidbody rb;
     private float gameTicks;
     public float horizSpeed;
+    private int test = 0;
+    private int test2 = 1;
+    public int distance;
 
     // Use this for initialization
     void Start () {
@@ -14,16 +17,28 @@ public class EnemyAI : MonoBehaviour {
         rb.freezeRotation = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+    void Update () {
         gameTicks += Time.deltaTime;
-        if (true)
+        if (test < distance && test2 == 1)
         {
             transform.Translate(Vector2.right * horizSpeed * Time.deltaTime);
+            test++;
         }
         else
         {
             transform.Translate(Vector2.left * horizSpeed * Time.deltaTime);
+            if (test2 == 1)
+            {
+                test2 = 0;
+            }
+            if (test > 0)
+            {
+                test--;
+            }
+            else
+            {
+                test2 = 1;
+            }
         }
 	}
 }
