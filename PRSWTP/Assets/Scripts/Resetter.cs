b8359db7751bb44d35c159a76cd82c22035f3cf7
Hazel <confirmationemailconfirmer@gmail.com>
2016-11-30 09:45:06 -0800

    recorded locations at the start

---
+++ PRSWTP/Assets/Scripts/Resetter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+    private Hashtable objects;
+
+	// Use this for initialization
+	void Start () {
+        objects = new Hashtable();
+        foreach (Transform child in transform)
+        {
+            objects.Add(child.name, child.position);
+        }
+        Debug.Log(objects);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
