921b57bb1b2de5b9d1468427013c7cb0154ce8d8
Hazel <confirmationemailconfirmer@gmail.com>
2016-12-02 09:06:07 -0800

    Added a suicide button.

--- PRSWTP/Assets/Scripts/EnemyAI.cs
+++ PRSWTP/Assets/Scripts/EnemyAI.cs
@@ -7,7 +7,7 @@ public class EnemyAI : MonoBehaviour {
     private Transform ts;
     private float gameTicks;
     public float horizSpeed;
-    private int test = 0;
+    //True is heading right, false is heading left
     private bool direction = true;
 
     public float rightEdge;
@@ -23,23 +23,22 @@ public class EnemyAI : MonoBehaviour {
 	
     void Update () {
         gameTicks += Time.deltaTime;
+        //Go right if you are not at the right edge yet, and you are headed right
         if (this.ts.position.x < rightEdge && direction)
         {
             transform.Translate(Vector2.right * horizSpeed * Time.deltaTime);
-            test++;
         }
+        //If you're at the right edge, or you're headed left, go left.
         else
         {
             transform.Translate(Vector2.left * horizSpeed * Time.deltaTime);
+            //You're headed left until you hit the edge, 
+            //then direction becomes true again and you go back right.
             if (direction)
             {
                 direction = false;
             }
-            if (this.ts.position.x > leftEdge)
-            {
-                test--;
-            }
-            else
+            if (this.ts.position.x < leftEdge)
             {
                 direction = true;
             }
