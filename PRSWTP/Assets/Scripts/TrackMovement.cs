ea39607deaef53dc86af7bd8b7ad37834ef4ea21
Cameron <evilbees@comcast.net>
2016-12-05 08:59:21 -0800

    b

--- PRSWTP/Assets/Scripts/TrackMovement.cs
+++ PRSWTP/Assets/Scripts/TrackMovement.cs
@@ -29,10 +29,11 @@ public class TrackMovement : MonoBehaviour {
         }
         test++;
         PlayerScript PlayerScript = GetComponent<PlayerScript>();
-        int charge = PlayerScript.resetCharges();
+        int charge = PlayerScript.getCharges();
         if (charge == 2)
         {
             Instantiate(player);
+            resetCharges();
         }
 	}
 }
