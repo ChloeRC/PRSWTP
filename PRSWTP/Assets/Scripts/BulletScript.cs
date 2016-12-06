4874278f6b109577935f8119c8b3bb564762d497
Hazel <confirmationemailconfirmer@gmail.com>
2016-12-06 09:40:13 -0800

    started bullet script

---
+++ PRSWTP/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    public float speed;
+
+    public bool dir;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dir == false)
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+        else
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+}
