63d7f5616f5e4b8070c17071d65e05fbd3f6300a
Minard <GAR_tsminard@seattleschools.org>
2017-01-04 10:23:16 -0800

    changed something

--- practice/Assets/Scripts/playerMovement.cs
+++ practice/Assets/Scripts/playerMovement.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class playerMovement : MonoBehaviour {
+    public Rigidbody rb; 
 
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, 0.0f, z);
+        rb.AddForce(movement);
 	}
 }
