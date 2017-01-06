c2c067958baa06daf5c4c777a37cbb09dd6ea2b9
Hazel <confirmationemailconfirmer@gmail.com>
2017-01-06 09:19:58 -0800

    Made sword toggle using PROPER MODEL.

--- PRSWTP/Assets/Scripts/SwordScript.cs
+++ PRSWTP/Assets/Scripts/SwordScript.cs
@@ -32,17 +32,17 @@ public class SwordScript : MonoBehaviour {
 
     void swordUp ()
     {
-        ts.localPosition = new Vector3(1.8f, 0.15f, 0);
-        Quaternion newRot = Quaternion.Euler(0, 0, -85);
+        ts.localPosition = new Vector3(1, 0.15f, 0);
+        Quaternion newRot = Quaternion.Euler(-10, 90, 90);
         ts.localRotation = new Quaternion(newRot.x, newRot.y, newRot.z, newRot.w);
-        ts.localScale = new Vector3(0.15f, 0.6f, 0.3f);
+        ts.localScale = new Vector3(0.055f, 0.1f, 0.1f);
     }
 
     void swordDown()
     {
-        ts.localPosition = new Vector3(0.2f, -0.175f, -0.7f);
-        Quaternion newRot = Quaternion.Euler(0, 0, 5);
+        ts.localPosition = new Vector3(0, 0.15f, -0.55f);
+        Quaternion newRot = Quaternion.Euler(78, 90, -90);
         ts.localRotation = new Quaternion(newRot.x, newRot.y, newRot.z, newRot.w);
-        ts.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        ts.localScale = new Vector3(0.09f, 0.1f, 0.04f);
     }
 }
