45143681b8adda9ba53430bee0da4625d0e381e5
Vismale <GAR_yavismale@seattleschools.org>
2017-02-15 10:36:57 -0800

    I made a title screen

---
+++ PRSWTP/Assets/Scripts/Main Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    bool newGame = false;
+    public GameObject NewGame;
+    public GameObject LoadGame;
+    public
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void GameSelection()
+    {
+        if (Ke)
+    }
+
+    }
+
