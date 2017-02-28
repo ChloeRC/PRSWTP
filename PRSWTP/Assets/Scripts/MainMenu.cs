c8b0b92fbeec5ccaee501be160232610806edf76
Vismale <GAR_yavismale@seattleschools.org>
2017-02-28 09:40:41 -0800

    Worked on intro scene.

---
+++ PRSWTP/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject NewGame;
+    public GameObject LoadGame;
+
+    void Start()
+    {
+        //  Application.LoadLevel("IntroScene");	
+    }
+
+    // Update is called once per frame
+    //Starts the first level after new game starts
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("FirstLevel");
+
+          //  Application.LoadLevel("FirstLevel");
+        }
+    }
+}
+
