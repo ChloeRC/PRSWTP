96ca27217f9fd7d4dd18f9275847064efa35ce97
Hazel <confirmationemailconfirmer@gmail.com>
2017-03-01 09:31:25 -0800

    Created a player representation and started the health bar.

---
+++ PRSWTP/Assets/Scripts/HealthBarDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarDisplay : MonoBehaviour {
+
+    public GameObject text;
+    public GameObject player;
+    private int health;
+    private int full;
+    
+	// Use this for initialization
+	void Start () {
+        health = player.GetComponent<PlayerScript>().health;
+        full = player.GetComponent<TrackMovement>().currLevelCharges();
+
+        text.GetComponent<TextMesh>().text = "Health: " + health + " / " + full;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateHealth()
+    {
+        text.GetComponent<TextMesh>().text = "Health: " + health + " / " + full;
+    }
+}
