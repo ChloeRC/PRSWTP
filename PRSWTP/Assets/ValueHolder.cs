53bdcefbe0df772f99dc3b9d95e8f453e0bef608
Hazel <confirmationemailconfirmer@gmail.com>
2017-03-24 09:56:50 -0700

    making valueholder work.

--- PRSWTP/Assets/ValueHolder.cs
+++ PRSWTP/Assets/ValueHolder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ValueHolder : MonoBehaviour {
 
     public static int checkpointNumber;
+    public static int currentCheckpoint = 0;
 
 	// Use this for initialization
 	void Start () {
