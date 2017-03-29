4b9aaacb5f29860fa86f5c69545c525d5e48834a
Hazel <confirmationemailconfirmer@gmail.com>
2017-03-29 10:41:37 -0700

    Attempting to get text to fade in on death screen.

---
+++ PRSWTP/Assets/LoadGameScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGameScript : MonoBehaviour {
+
+    private Color transparent;
+    private Color opaque;
+    private float timer;
+    private float currOpacity;
+
+    // Use this for initialization
+    void Start()
+    {
+        transparent = new Color(1, 1, 1, 0);
+        opaque = new Color(1, 1, 1, 1);
+        timer = 0f;
+        currOpacity = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > .1)
+        {
+            //GetComponent<Text>().color = new Color(1, 1, 1, currOpacity);
+            currOpacity += .1f;
+            timer = 0f;
+        }
+	}
+}
