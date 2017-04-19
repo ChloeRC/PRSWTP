1ddf38220ff86fa44aa693340d49183f18f7adae
Hazel <confirmationemailconfirmer@gmail.com>
2017-04-19 10:23:16 -0700

    made an insta-kill function.

---
+++ PRSWTP/Assets/InstaKillScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaKillScript : MonoBehaviour {
+
+    public GameObject enemy;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            enemy.GetComponent<BossAI>().kill();
+        }
+    }
+}
