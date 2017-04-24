5d5ef8099baac4b7609ff3497dd97abc79b7cde8
Zeng <GAR_awzeng@seattleschools.org>
2017-04-24 09:35:27 -0700

    wait

--- PRSWTP/Assets/Scripts/PlayerScript.cs
+++ PRSWTP/Assets/Scripts/PlayerScript.cs
@@ -256,6 +256,8 @@ public class PlayerScript : MonoBehaviour {
         //When the player resets, there's this weird thing where position is -21 for a bit????
         if (health <= 0 || GetComponent<Transform>().position.y <= -25f)
         {
+            Debug.Log("Health: " + health);
+            Debug.Log("Position" + GetComponent<Transform>().position.y);
             Debug.Log("bop bop bop to the top");
             kill();
         }
