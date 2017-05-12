2d2e81042c7f811dec12757dc1d982eb910f1339
Hazel <confirmationemailconfirmer@gmail.com>
2017-05-12 09:53:34 -0700

    the past selves now know when they would shoot/stab, but they don't, yet

--- PRSWTP/Assets/Scripts/PlayerScript.cs
+++ PRSWTP/Assets/Scripts/PlayerScript.cs
@@ -71,6 +71,10 @@ public class PlayerScript : MonoBehaviour {
     public GameObject nonPlayerObjects;
     private GameObject info; //stores information across time travel
 
+    //These two store when you shoot/stab something so it can be replicated by the past self
+    public bool didSword;
+    public bool didShoot;
+
     // Use this for initialization
     void OnLevelWasLoaded()
     {
@@ -101,6 +105,10 @@ public class PlayerScript : MonoBehaviour {
         //INITIALIZES THE COOLDOWN TIMERS
         hasShot = shotCooldown;
         hasSword = 0;
+
+        //INITIALIZES THE SWORD/GUN MONITORS
+        didSword = false;
+        didShoot = false;
         
         gunBarDisplay.GetComponent<GunBarDisplay>().UpdateText();
     }
@@ -184,6 +192,9 @@ public class PlayerScript : MonoBehaviour {
                 //The sword increments upwards from 1 to swordCooldown, then gets set to 0. 0 is usable.
                 hasSword = 1;
                 sword.drawn = true;
+
+                //Marks the monitor as used
+                didSword = true;
             }
 
             //REDRAW THE SWORD TO ACCOUNT FOR RECENT CHANGES
@@ -229,6 +240,9 @@ public class PlayerScript : MonoBehaviour {
                 var newBullet = Instantiate(bullet, pos, Quaternion.Euler(0, 0, rotation));
                 var rbBullet = newBullet.GetComponent<Rigidbody>();
                 rbBullet.velocity = newBullet.GetComponent<BulletScript>().speed * force;
+
+                //Marks the monitor as used
+                didShoot = true;
             }
 
             //Tell if there is anything in a sphere shape below the player
