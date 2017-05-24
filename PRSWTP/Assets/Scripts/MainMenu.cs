35daa60f68c982fdaa78267fdd9d7bb9cf73f6b3
Hazel <confirmationemailconfirmer@gmail.com>
2017-05-24 10:06:14 -0700

    editing debug messages

--- PRSWTP/Assets/Scripts/MainMenu.cs
+++ PRSWTP/Assets/Scripts/MainMenu.cs
@@ -20,8 +20,6 @@ public class MainMenu : MonoBehaviour
         if (checkpointNumber != 0)
         {
             ValueHolder.checkpointNumber = this.checkpointNumber;
-
-            Debug.Log("this checkpoint");
         }
 
         SceneManager.LoadScene(goTo);
