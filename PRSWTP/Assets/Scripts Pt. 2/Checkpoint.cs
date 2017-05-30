608b542375ed261322e47834fae98e637d3765ac
Vismale <GAR_yavismale@seattleschools.org>
2017-05-30 09:29:55 -0700

    g'

--- PRSWTP/Assets/Scripts Pt. 2/Checkpoint.cs
+++ PRSWTP/Assets/Scripts Pt. 2/Checkpoint.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
 
 public class Checkpoint : MonoBehaviour {
 
     public int number;
+    void Start()
+    {
+        
+    } 
 
     public void lightOn()
     {
         transform.GetChild(1).GetComponent<Light>().intensity = 8;
     }
 
-    public void lightOff()
+    public void update()
     {
+        GetComponent<AudioSource>().Play();
         transform.GetChild(1).GetComponent<Light>().intensity = 0;
     }
 }
