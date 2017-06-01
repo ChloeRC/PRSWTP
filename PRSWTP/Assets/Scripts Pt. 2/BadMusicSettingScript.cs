9fa46b797c50d40f5a4374d1c36168d5988e3f53
Aileen <aileen.w.zeng@gmail.com>
2017-06-01 10:40:19 -0700

    ughghhghghgh

---
+++ PRSWTP/Assets/Scripts Pt. 2/BadMusicSettingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadMusicSettingScript : MonoBehaviour {
+
+	public bool toggleTo;
+	private static readonly string MUSIC = "music";
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton (MUSIC) = true) {
+			ValueHolder.music = toggleTo;
+			//toggleTo != toggleTo;
+		}
+		//if the music is set to this button, turn this button green
+		if (toggleTo == ValueHolder.music)
+		{
+			GetComponent<TextMesh>().color = Color.green;
+		}
+		else //else turn it white
+		{
+			GetComponent<TextMesh>().color = Color.white;
+		}
+	}
+}
