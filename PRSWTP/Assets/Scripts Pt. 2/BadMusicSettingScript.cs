b88a15d798634be940e01fc1485910ff052a1007
Aileen <aileen.w.zeng@gmail.com>
2017-06-01 12:10:35 -0700

    MUTE MUSIC WORKS but not sounds... also it's toggled by the m key because I hate onclick things

--- PRSWTP/Assets/Scripts Pt. 2/BadMusicSettingScript.cs
+++ PRSWTP/Assets/Scripts Pt. 2/BadMusicSettingScript.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class BadMusicSettingScript : MonoBehaviour {
 
 	public bool toggleTo;
+	private float gameTicks;
 	private static readonly string MUSIC = "music";
 		
 	// Use this for initialization
 	void Start () {
+		gameTicks = 0.0f;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton (MUSIC) == true) {
+		gameTicks += Time.deltaTime;
+		//press the m button
+		if (Input.GetButton (MUSIC) == true && gameTicks > 0.2f) 
+		{
+			toggleTo = !toggleTo;
 			ValueHolder.music = toggleTo;
-			//toggleTo != toggleTo;
+			Debug.Log (ValueHolder.music);
+			gameTicks = 0.0f;
 		}
 		//if the music is set to this button, turn this button green
 		if (toggleTo == ValueHolder.music)
--- PRSWTP/Assets/Scripts/SoundSphereScript.cs
+++ PRSWTP/Assets/Scripts/SoundSphereScript.cs
@@ -6,10 +6,18 @@ public class SoundSphereScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+	}
+
+	void Update() {
         //defaults to playing music, only if not...
 		if (!ValueHolder.music)
         {
             GetComponent<AudioSource>().mute = true;
         }
+
+		if (ValueHolder.music) 
+		{
+			GetComponent<AudioSource> ().mute = false;
+		}
 	}
 }
